f3709a264b0302c57dc114f1fe3b12d7a761cd12
Saul Castellanos <saulcastel018@gmail.com>
2024-08-07 19:38:50 -0600

    [ADD] drizzle-orm

---
+++ Backend/src/controllers/ClientController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import { clients } from '../database/schema.js';
+import { MySql2Database } from 'drizzle-orm/mysql2';
+import { eq } from 'drizzle-orm';
+
+export default class clientsController {
+    db: MySql2Database;
+    constructor(db: MySql2Database) {
+        this.db = db;
+    }
+
+    async getClients(req: Request, res: Response) {
+        const result = await this.db.select().from(clients);
+        res.status(200).send(result);
+    }
+
+    getClient(req: Request, res: Response) {
+        res.send(
+            'Aquí iría la implementación de este método... ¡SI TUVIERA UNA!'
+        );
+    }
+
+    async createClient(req: Request, res: Response) {
+        const result = await this.db
+            .insert(clients)
+            .values(req.body)
+            .$returningId();
+        res.status(200).send(result);
+    }
+
+    async updateClient(req: Request, res: Response) {
+        await this.db
+            .update(clients)
+            .set(req.body)
+            .where(eq(clients.cui, Number(req.params.id)));
+        res.status(200).send('');
+    }
+}
---
+++ Backend/src/database/connection.ts
@@ -0,0 +1,12 @@
+import { drizzle } from 'drizzle-orm/mysql2';
+import mysql from 'mysql2/promise';
+
+const connection = await mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+});
+
+export const db = drizzle(connection);
