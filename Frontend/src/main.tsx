7c3e5214b49114cc4ba1e9b4f2e9bc5dc23b6928
Xhunik Miguel <xhuniktzi@gmail.com>
2024-08-07 19:01:53 -0600

    201900462 - routing base

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Login from './routes/auth/Login'
+import ErrorPage from './routes/error'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/auth/login",
+    element: <Login />
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
