0b9227a399c6eaffd621ef4d0e325dbca1fab54f
Saul Castellanos <saulcastel018@gmail.com>
2024-08-09 10:11:12 -0600

    [ADD] get all clients endpoint

--- Backend/src/routes/ClientRoutes.ts
+++ Backend/src/routes/ClientRoutes.ts
@@ -4,11 +4,14 @@ import ClientController from '../controllers/ClientController.js';
 const clientRouter = express.Router();
 const clientController = new ClientController();
 
-clientRouter.post('/', clientController.createClient);
+clientRouter
+    .route('/')
+    .get(clientController.getClients)
+    .post(clientController.createClient);
+
 clientRouter
     .route('/:id')
     .get(clientController.getClient)
     .put(clientController.updateClient);
-
 
 export default clientRouter;
